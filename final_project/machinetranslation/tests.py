b068933eac03543f51bade63943065d6fa7da5ec
Ryllian <ryllian@hotmail.com>
2022-02-19 02:05:11 +0000

    step 4 complete

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,11 +1,20 @@
 import unittest
-
-import translator.py
+from translator import english_to_french, french_to_english
 
 class testEnglishToFrench(unittest.TestCase): 
-    def nullTest(self): 
-        self.assertEqual(englishToFrench(None), None) # test when 2 is given as input the output is 4.
-    def helloTest(self):
-        self.assertEqual(
+    # Null test
+    def test1(self): 
+        self.assertNotEqual(english_to_french(None), None)
+    # Hello input test
+    def test2(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
 
 class testFrenchToEnglish(unittest.TestCase): 
+    # Null test
+    def test1(self): 
+        self.assertNotEqual(french_to_english(None), None)
+    # Bonjour inpuit test
+    def test2(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
+import os
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,24 +17,24 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
 
-def englishToFrench(englishText):
-    translation = language_translator.translate(text=englishText, model_id='en-fr').get_result()
-    print(json.dumps(translation, indent=2, ensure_ascii=False))
+def english_to_french(englishText):
+    """Input str in english and output str in french"""
     try:
+        translation = language_translator.translate(text=englishText, model_id='en-fr').get_result()
+        # print(json.dumps(translation, indent=2, ensure_ascii=False))
         frenchText = translation['translations'][0]['translation']
-    except:
-        return None
+    except ValueError:
+        return ""
+    print("Input text: " + englishText + '\n' + "Translated text: " + frenchText)
     return frenchText
 
-def frenchToEnglish(frenchText):
-    translation = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
-    print(json.dumps(translation, indent=2, ensure_ascii=False))
-    englishText = translation['translations'][0]['translation']
+def french_to_english(frenchText):
+    """Input str in french and output str in english"""
     try:
-        frenchText = translation['translations'][0]['translation']
-    except:
-        return None
+        translation = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
+        # print(json.dumps(translation, indent=2, ensure_ascii=False))
+        englishText = translation['translations'][0]['translation']
+    except ValueError:
+        return ""
+    print("Input text: " + frenchText + '\n' + "Translated text: " + englishText)
     return englishText
-
-
-    
