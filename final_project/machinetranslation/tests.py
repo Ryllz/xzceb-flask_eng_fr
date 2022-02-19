73eb185e0dbfa7caf1e8d5d268902e8dd61f27b6
Ryllian <ryllian@hotmail.com>
2022-02-19 01:32:53 +0000

    initial commit

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+import translator.py
+
+class testEnglishToFrench(unittest.TestCase): 
+    def nullTest(self): 
+        self.assertEqual(englishToFrench(None), None) # test when 2 is given as input the output is 4.
+    def helloTest(self):
+        self.assertEqual(
+
+class testFrenchToEnglish(unittest.TestCase): 
